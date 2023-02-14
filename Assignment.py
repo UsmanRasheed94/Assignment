8254c4cd0bffa312203ef0218205f509a2110e5b
UsmanRasheed94 <124665044+UsmanRasheed94@users.noreply.github.com>
2023-02-14 15:40:18 +0500

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>My Jupyter Notebook on IBM Watson Studio</h1>
+
+# <p><b>Usman Rasheed</b></p>
+# <p>Professor</p>
+
+# <p><i>I am interested in data sciences because of its vast use and demand in current research and idustry. Furthermore, I want to pursue my PhD in data sciences and machine learning. That is the reason for me to take this course.</i></p>
+
+# <h3> Code in the next cell will show the addition of two numbers.</h3>
+
+# In[2]:
+
+
+1 + 2
+
+
+# <h2>Bulleted List</h2>
+# 
+# <ul>
+#   <li>Coffee</li>
+#   <li>Tea</li>
+#   <li>Milk</li>
+# </ul>  
+# 
+# <h2>Numbered List</h2>
+# 
+# <ol>
+#   <li>Coffee</li>
+#   <li>Tea</li>
+#   <li>Milk</li>
+# </ol> 
+#  <table>
+#   <tr>
+#     <th>Class</th>
+#     <th>Section</th>
+#     <th>Subject</th>
+#   </tr>
+#   <tr>
+#     <td>BSCS</td>
+#     <td>A</td>
+#     <td>OOP</td>
+#   </tr>
+#   <tr>
+#     <td>BSCS</td>
+#     <td>B</td>
+#     <td>Database</td>
+#   </tr>
+# </table> 
+# 
+
+# In[ ]:
+
+
+
+
